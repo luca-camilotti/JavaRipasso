961ceb9982762dbd1d681679e6fee87f03e1be60
luca-camilotti <luca.camilotti@gmail.com>
2021-09-23 09:47:05 +0200

    main class added

---
+++ src/my/java/exercise/MainClass.java
@@ -0,0 +1,11 @@
+package my.java.exercise;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Git!");
+
+	}
+
+}
