60c62463ba9a8f9b3a2b85eb1f0716d266073f6f
luca-camilotti <luca.camilotti@gmail.com>
2021-09-24 15:00:51 +0200

    first thread example

--- src/my/java/exercise/MainClass.java
+++ src/my/java/exercise/MainClass.java
@@ -5,6 +5,14 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
+		
+		// The Traditional Long Way:
+		Task j = new Task();
+		Thread t = new Thread(j);
+		t.start();
+		
+		// Oggetti anonimi:
+		new Thread(new Task()).start();
 
 	}
 
